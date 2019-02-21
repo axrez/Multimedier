a9e871c9764ddefd24347eb239bd1d225044e0e5
axrez <isdragon112@gmail.com>
2019-02-21 09:21:19 +0100

    git test

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Hej Justin
           </a>
         </header>
       </div>
